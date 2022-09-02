22533f79779105de78232f2b82509c2e131758ab
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2022-09-02 23:56:28 +0500

    commit

---
+++ home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:practiceflutter/screen_two.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Navigation Drawer"),
+        backgroundColor: Colors.teal,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ScreenTwo(),
+                ),
+              );
+            },
+            child: const Center(
+              child: Text(
+                "Screen 1",
+                style: TextStyle(fontSize: 25),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
