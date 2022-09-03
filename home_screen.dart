fbda16a1d9a69db9a00ed8144241136a0f79047d
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2022-09-03 05:59:50 +0500

    update

--- home_screen.dart
+++ home_screen.dart
@@ -41,3 +41,54 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+
+
+
+
+
+
+// Using (Method-2)
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:practiceflutter/screen_two.dart';
+
+// class HomeScreen extends StatefulWidget {
+//   static const String id = 'home_screen';
+//   const HomeScreen({super.key});
+
+//   @override
+//   State<HomeScreen> createState() => _HomeScreenState();
+// }
+
+// class _HomeScreenState extends State<HomeScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Navigation Drawer"),
+//         backgroundColor: Colors.teal,
+//       ),
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         crossAxisAlignment: CrossAxisAlignment.center,
+//         children: [
+//           TextButton(
+//             onPressed: () {
+//               Navigator.pushNamed(context, ScreenTwo.id);
+//             },
+//             child: const Center(
+//               child: Text(
+//                 "Screen 1",
+//                 style: TextStyle(fontSize: 25),
+//               ),
+//             ),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
+
