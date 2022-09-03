e62ebcaae525291e5475af9586792af53969c02c
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2022-09-03 05:58:20 +0500

    update

--- main.dart
+++ main.dart
@@ -21,3 +21,38 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+
+
+
+
+// Using (Method-2)
+
+
+// import 'package:flutter/material.dart';
+// import 'package:practiceflutter/home_screen.dart';
+// import 'package:practiceflutter/screen_two.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       initialRoute: HomeScreen.id,
+//       routes: {
+//         HomeScreen.id: (context) => const HomeScreen(),
+//         ScreenTwo.id: (context) => const ScreenTwo(),
+//       },
+//     );
+//   }
+// }
